e917bf367674c698172fb094d5fdc9ceba819a40
Ashish Aggarwal <aggarwal.ash@husky.neu.edu>
2020-02-25 16:31:10 -0500

    Checkpoint2

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static public string DefaultConnectionString { get; } = @"Server=(localdb)\\mssqllocaldb;Database=SampleData";
+        static IReadOnlyDictionary<string, string> DefaultConnectionStrings { get; } =
+            new Dictionary<string, string>()
+            {
+                ["Profile:Username"] = Environment.UserName,
+                ["Window:Width"] = "50",
+                ["Window:Height"] = "5",
+                [$"AppConfiguration:ConnectionString"] = DefaultConnectionString,
+            };
+        static public IConfiguration Configuration { get; set; }
+
+        static void Main(string[] args)
+        {
+            ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            configurationBuilder.AddInMemoryCollection(DefaultConnectionStrings);
+            configurationBuilder.AddJsonFile("appsettings.json",true,true);
+            configurationBuilder.AddCommandLine(args);
+
+            Configuration = configurationBuilder.Build();
+            Console.SetWindowSize(Int32.Parse(Configuration["AppConfiguration:MainWindow:Width"]), Int32.Parse(Configuration["AppConfiguration:MainWindow:Height"]));
+            Console.WriteLine($"Hello { Configuration.GetValue<string>("Profile:Username")}");
+
+            Console.WriteLine($"Top { Configuration.GetValue<string>("AppConfiguration:MainWindow:Top")}");
+            Console.WriteLine($"Hello { Configuration.GetValue<string>("firstname")}");
+            Console.ReadKey();
+        }
+    }
+}
